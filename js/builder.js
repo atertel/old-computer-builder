469454f4abae9989c6c3798f66f5747f76b98212
alexander <alexanderertel@gmail.com>
2016-10-05 16:54:06 -0400

    working on mobo stage

--- js/builder.js
+++ js/builder.js
@@ -5,17 +5,13 @@ var glowColor = "blue";
 
 // Motherboard Canvas
 function highlightMotherboard () {
-    var mobo_canvas = document.getElementById('moboCanvas').getContext('2d');
-    var mobo_img = document.getElementById('moboImage');
+    var canvas = document.getElementById("moboCanvas");
+    var context = canvas.getContext('2d');
+    var img = new Image();
 
-    mobo_canvas.save();
-    mobo_canvas.strokeStyle = glowColor;
-    mobo_canvas.shadowColor = glowColor;
-    mobo_canvas.shadowOffsetX = 300;
-    for (var i = 0; i < 10; i++) {
-        mobo_canvas.shadowBlur = i * 2;
-        mobo_canvas.strokeRect(-270, 30, 5, 150);
-    }
-    mobo_canvas.restore();
+    img.onload = function() {
+        context.drawImage(img, 50, 50);
+    };
+    img.src = "img/motherboard.jpg";
 
 }
