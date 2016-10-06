e4ecac6be9b34f02da1433ab75e8d4ed08a51427
alexander <alexanderertel@gmail.com>
2016-10-06 16:13:37 -0400

    more mobo work and design changes

--- js/builder.js
+++ js/builder.js
@@ -1,17 +1,21 @@
 // THIS FILE IS USED TO CREATE ALL THE CANVASES, PART IMAGES, ETC.
+/*
 
-//for setting outline glow
-var glowColor = "blue";
+ <!-- <canvas id="mobo-canvas" width=300 height=300></canvas>
+ <script>
+ var canvas = document.getElementById("mobo-canvas");
+ var ctx = canvas.getContext("2d");
+ var glowColor="blue";
 
-// Motherboard Canvas
-function highlightMotherboard () {
-    var canvas = document.getElementById("moboCanvas");
-    var context = canvas.getContext('2d');
-    var img = new Image();
-
-    img.onload = function() {
-        context.drawImage(img, 50, 50);
-    };
-    img.src = "img/motherboard.jpg";
-
-}
+ ctx.save();
+ ctx.globalCompositeOperation = "source-over";
+ ctx.strokeStyle = glowColor;
+ ctx.shadowColor = glowColor;
+ ctx.shadowOffsetX=300;
+ for (var i = 0; i < 10; i++) {
+ ctx.shadowBlur = i * 2;
+ ctx.strokeRect(-300, 100, 175, 175);
+ }
+ ctx.restore();
+ </script> -->
+ */
